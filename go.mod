33eb2bcb222b9601aea246bfaa90039ec17568be
teruya <teru0x01.sheep@gmail.com>
2020-05-05 22:56:27 +0900

    add func signatures

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/teru01/serv-start
 go 1.14
 
 require (
-	github.com/jessevdk/go-flags v1.4.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0
 	github.com/lestrrat/go-server-starter v0.0.0-20180220115249-6ac0b358431b
 )
--- starter.go
+++ starter.go
@@ -1,3 +1,93 @@
 package starter
 
+import (
+	"net"
+	"os"
+	"syscall"
+	"time"
+)
+
+var niceSigNames map[syscall.Signal]string
+var niceNameToSigs map[string]syscall.Signal
+var successStatus syscall.WaitStatus
+var failureStatus syscall.WaitStatus
+
+func makeNiceSigNamesCommon() map[syscall.Signal]string {
+
+}
+
+func makeNiceSigNames() map[syscall.Signal]string {
+
+}
+
+func init() {
+
+}
+
+type listener struct {
+	listener net.Listener
+	spec string
+}
+
+type Config interface {
+	Args() []string
+	Command() string
+	Dir() string
+	Interval() time.Duration
+	PidFile() string
+	Ports() []string
+	Paths() []string
+	SignalOnHUP() os.Signal
+	SignalOnTERM() os.Signal
+	StatusFile() string
+}
+
+type Starter struct {
+	interval time.Duration
+	signalOnHUP os.Signal
+	signalOnTERM os.Signal
+	statusFile string
+	pidFile    string
+	dir        string
+	ports      []string
+	paths      []string
+	listeners  []listener
+	generation int
+	command    string
+	args       []string
+}
+
+func NewStarter(c Config) (*Starter, error) {
+
+}
+
+func (s Starter) Stop() {
+
+}
+
+type processState interface {
+	Pid() int
+	Sys() interface{}
+}
+
+type dummyProcessState struct {
+	pid int
+	status syscall.WaitStatus
+}
+
+func grabExitStatus(st processState) syscall.WaitStatus {
+
+}
+
+func (d dummyProcessState) Pid() int {
+	return d.Pid()
+}
+
+func (d dummyProcessState) Sys() interface{} {
+	return d.status
+}
+
+func signame(s os.Signal) string {
+	
+}
 
