e90a1ccece6968c223028445e4723880cc9b23b2
teruya <teru0x01.sheep@gmail.com>
2020-05-06 16:16:53 +0900

    update

--- starter_any.go
+++ starter_any.go
@@ -8,13 +8,36 @@ import (
 )
 
 func init() {
-
+	failureStatus = syscall.WaitStatus(255)
+	successStatus = syscall.WaitStatus(0)
 }
 
 func addPlatformDependentNiceSignalNames(v map[syscall.Signal]string) map[syscall.Signal]string {
-
+	v[syscall.SIGCHLD] = "CHLD"
+	v[syscall.SIGCONT] = "CONT"
+	v[syscall.SIGIO] = "IO"
+	v[syscall.SIGPROF] = "PROF"
+	v[syscall.SIGSTOP] = "STOP"
+	v[syscall.SIGSYS] = "SYS"
+	v[syscall.SIGTSTP] = "TSTP"
+	v[syscall.SIGTTIN] = "TTIN"
+	v[syscall.SIGTTOU] = "TTOU"
+	v[syscall.SIGURG] = "URG"
+	v[syscall.SIGUSR1] = "USR1"
+	v[syscall.SIGUSR2] = "USR2"
+	v[syscall.SIGVTALRM] = "VTALRM"
+	v[syscall.SIGWINCH] = "WINCH"
+	v[syscall.SIGXCPU] = "XCPU"
+	v[syscall.SIGXFSZ] = "GXFSZ"
+	return v
 }
 
 func findWorker(pid int) *os.Process {
-
+	var wstatus syscall.WaitStatus
+	waitpid, _ := syscall.Wait4(pid, &wstatus, syscall.WNOHANG, nil)
+	if waitpid <= 0 {
+		p, _ := os.FindProcess(pid)
+		return p
+	}
+	return nil
 }
